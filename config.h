f8c090d37189b6ce240208cbbcf3713747a47c04
Gabriel Coronel <gabrielcoronel0303@gmail.com>
2021-03-29 09:12:30 -0600

    Fix typo in rules section

--- config.h
+++ config.h
@@ -27,7 +27,7 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ NULL,       NULL,       NULL,       O,            0,           -1 },
+	{ NULL,       NULL,       NULL,       0,            0,           -1 },
 };
 
 /* layout(s) */
