6996fa08199e4a2157fc67e799062daa5dc82f68
Gabriel Coronel <gabrielcoronel0303@gmail.com>
2021-03-30 13:59:00 -0600

    Disable size hints

--- config.h
+++ config.h
@@ -32,7 +32,7 @@ static const int follow_tag = 1; /* Non-zero means to follow when a client is se
 /* layout(s) */
 static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
