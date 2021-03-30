766c34512bd61383554358633623f66a3b627539
Gabriel Coronel <gabrielcoronel0303@gmail.com>
2021-03-30 01:46:49 -0600

    Get rid of the spawn hotkeys

--- config.h
+++ config.h
@@ -52,16 +52,13 @@ static const Layout layouts[] = {
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
-/* commands */
-// static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-// static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char* applauncher_command[] = { "rofi", "-show", NULL };
-static const char* terminal_command[]  = { "alacritty", NULL };
+/*
+  Note: the spawn hotkeys were moved to the sxhkd config file,
+  that way they still WM-independent
+*/
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = applauncher_command } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = terminal_command } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
